92d1438570b141ede9d5cef731187b6bae9366f0
AyaAboZied <68289149+AyaAboZied@users.noreply.github.com>
2023-01-28 23:29:38 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+# get the public ip of the public ec2 to see if apache install without going to console
 output "instance_ip_addr" {
-  value = aws_instance.tf-ec2-public.public_ip
+  value = aws_instance.tf-ec2-public[0].public_ip
 }
